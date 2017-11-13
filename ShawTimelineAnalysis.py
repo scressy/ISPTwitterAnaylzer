9a7ac311c15ba111ddefc65c54da1be5c2c69c37
Hannah Wright <Heenawter@users.noreply.github.com>
2017-11-12 18:03:58 -0700

    working on getting avg tweets per day in a week

--- ShawTimelineAnalysis.py
+++ ShawTimelineAnalysis.py
@@ -12,7 +12,6 @@ import csv
 import json
 import got
 
-####input your credentials here
 consumer_key = 'ejCkcbYsjKRCI6e125bJpG49x'
 consumer_secret = 'qqEeJUPeFdlZyETjx6hJxfFw6i1gYWCINpQvdwdJzh7rMKttTu'
 access_token = '919666743655538688-bCIJ3vxitH4jX1XE7xj1sUniYGR5pjH'
@@ -38,8 +37,17 @@ def get_tweet_sentiment(tweet):
     else:
         return 'negative'
 
+# Source: https://stackoverflow.com/questions/6170246/how-do-i-use-matplotlib-autopct
+def make_autopct(values):
+    def my_autopct(pct):
+        total = sum(values)
+        val = int(round(pct*total/100.0))
+        return '{p:.2f}%  ({v:d})'.format(p=pct,v=val)
+    return my_autopct
+
 # Reference: https://plot.ly/matplotlib/bar-charts/#matplotlib-bar-chart-with-dates
-def sentiment_numbers(user,tweets):
+# Creates a pie chart of postive/negative/neutral for tweets
+def plot_sentiment_numbers(source,tweets):
     ptweets = len([tweet for tweet in tweets if get_tweet_sentiment(tweet.text) == 'positive'])
     ntweets = len([tweet for tweet in tweets if get_tweet_sentiment(tweet.text) == 'negative'])
     numNeutral = len(tweets)-ntweets-ptweets
@@ -50,7 +58,7 @@ def sentiment_numbers(user,tweets):
     fig = plt.figure(figsize=(6,6))
 
     plt.axis("equal")
-    patches, texts, autotexts = plt.pie(vals, labels=sentiments, autopct='%1.1f%%')
+    patches, texts, autotexts = plt.pie(vals, labels=sentiments, autopct=make_autopct(vals))
     plt.title('Number of Tweets by Sentiment', fontsize=18)
 
     for t in texts:
@@ -58,17 +66,35 @@ def sentiment_numbers(user,tweets):
     for t in autotexts:
         t.set_size(16)
 
-    fig.savefig('plots/' + user + '_sentiment_numbers')
+    fig.savefig('plots/' + source + '_sentiment_numbers')
 
     plt.tight_layout()
     plt.show()
 
-# users = ['ShawHelp','ShawInfo']
+def plot_tweets_per_day(source,tweets):
+    fig = plt.figure(figsize=(6,6))
+    plt.show()
 
-users = ['ShawHelp']
+users = ['ShawHelp','ShawInfo']
+startDate = '2017-10-01'
+endDate = '2017-11-11'
 
-for user in users:
-    tweetCriteria = got.manager.TweetCriteria().setUsername(user).setSince("2017-10-11").setUntil("2017-10-12")
+def sentiment_analysis():
+    for user in users:
+        tweetCriteria = got.manager.TweetCriteria().setUsername(user).setSince(startDate).setUntil(endDate)
+        shawTweets = got.manager.TweetManager.getTweets(tweetCriteria)
+
+        plot_sentiment_numbers(user,shawTweets)
+
+    tweetCriteria = got.manager.TweetCriteria().setQuerySearch('#ShawInternet').setSince(startDate).setUntil(endDate)
     tweets = got.manager.TweetManager.getTweets(tweetCriteria)
 
-    sentiment_numbers(user,tweets)
+    plot_sentiment_numbers("hastag_shawInternet",tweets)
+
+def avg_tweets_per_day():
+    tweetCriteria = got.manager.TweetCriteria().setQuerySearch('#ShawInternet').setSince(startDate).setUntil(endDate)
+    tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+
+    plot_tweets_per_day('beepnoop',tweets)
+
+avg_tweets_per_day()
